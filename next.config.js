19e2a682bc3bb4721853b9540ba2b9aa37b08674
aryan <agarwalaryan139@gmail.com>
2023-04-30 02:47:11 +0530

    lol key added in env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     NFT_STORAGE_API_KEY: process.env.NFT_STORAGE_API_KEY,
     UNDERDOG_PROTOCOL_API_KEY: process.env.UNDERDOG_PROTOCOL_API_KEY,
     SOLANA_PRIVATE_KEY: process.env.SOLANA_PRIVATE_KEY,
+    HELIUS_KEY: process.env.HELIUS_KEY,
   },
 }
 
