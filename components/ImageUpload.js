38622e09c92275b0a579184be88ce44698ce197e
aryan <agarwalaryan139@gmail.com>
2023-04-29 15:40:04 +0530

    ui improvement

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Box, Button, Image, Input , VStack} from '@chakra-ui/react';
+import { Box, Button, Image, Input , Text, VStack} from '@chakra-ui/react';
 import uploadToIPFS from '@/functions/uploadToIPFS';
 import uploadToUnderdog from '@/functions/uploadToUnderdog';
 
 function ImageUpload() {
   const [image, setImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [ipfsHash, setIpfsHash] = useState(null);
 
   const handleImageChange = (e) => {
     if (e.target.files && e.target.files[0]) {
@@ -18,13 +20,16 @@ function ImageUpload() {
 
   const handleUpload = async () => {
     const imageBuffer = Buffer.from(image.split(',')[1], 'base64');
-    const ipfsHash = await uploadToIPFS(imageBuffer);
-    const underdogHash = await uploadToUnderdog();
+    setIsLoading(true);
+    const ipfsHashe = await uploadToIPFS(imageBuffer);
+    setIsLoading(false);
+    setIpfsHash(ipfsHashe)
     console.log(ipfsHash);
   };
 
   return (
     <VStack m={30} gap={60}>
+      <Text fontSize="6xl">create your own NFTs for FREE</Text>
       {image && <Image src={image} height={300} width={300} alt="uploaded image" />}
       {!image && (
         <Box
@@ -34,7 +39,14 @@ function ImageUpload() {
           borderRadius="md"
         ></Box>
       )}
-      {image && <Button onClick={handleUpload}>Upload Image</Button>}
+      {ipfsHash && <Text>
+        Your NFT Metadata is available at: <a href={ipfsHash}>Click here</a>
+        </Text>}
+      
+      {image && !isLoading && !ipfsHash && (
+        <Button onClick={handleUpload}>Upload to IPFS</Button>
+      )}
+      {image && isLoading && <Button isLoading disabled>Uploading to IPFS...</Button>}
       {image && <Button onClick={() => setImage(null)}>Remove Image</Button>}
       <Input type="file" onChange={handleImageChange} />
     </VStack>   
